aa8874483c5d03b97b25a1bfc582c797804a62f9
franciscocm22 <franciscocm22@TTC-204-26>
2017-01-19 19:52:59 +0100

    Simple timer added.

---
+++ SimpleTimer.h
@@ -0,0 +1,44 @@
+#ifndef __Timer_H__
+#define __Timer_H__
+
+#include "SDL\include\SDL.h"
+
+class SimpleTimer
+{
+public:
+	SimpleTimer()
+	{}
+
+	Uint32 start() 
+	{
+		init = SDL_GetTicks();
+		return init;
+	}
+
+	Uint32 read() 
+	{
+		if (stopTimer == true)
+			return time;
+		return SDL_GetTicks() - init;
+	}
+
+	Uint32 stop()
+	{
+		if (stopTimer == false)
+			time = SDL_GetTicks() - init;
+		stopTimer = true;
+		return time;
+	}
+	void reset()
+	{
+		stopTimer = false;
+		init = 0;
+	}
+
+public:
+	bool stopTimer;
+	Uint32 time,init;
+
+};
+
+#endif
