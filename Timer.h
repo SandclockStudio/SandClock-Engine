7af4d29f789b4f5e93a704e14532c8ed501c06c0
sliz3r <davidmarti.mensajes@gmail.com>
2017-01-19 19:47:45 +0100

    Modified Timer class.

--- Timer.h
+++ Timer.h
@@ -8,15 +8,38 @@ class Timer
 {
 	public:
 
-		Timer();
-		~Timer();
-
-		void setFirstTime();
-		void setSecondTime();
-		Uint64 getTime();
+		Uint64 t1,t2;
+		bool stopTimer = true;
 
 	public:
-		Uint64 t1, t2;
+
+		Timer()
+		{}
+
+		~Timer()
+		{}
+
+		void start()
+		{
+			stopTimer = false;
+			t1 = SDL_GetPerformanceCounter();
+		}
+
+		Uint64 read()
+		{
+			if (!stopTimer)
+				return (double)((SDL_GetPerformanceCounter() - t1) * 1000 / SDL_GetPerformanceFrequency());
+			else
+				return t2;
+		}
+
+		Uint64 stop()
+		{
+			stopTimer = true;
+			t2 = (double)((SDL_GetPerformanceCounter() - t1) * 1000 / SDL_GetPerformanceFrequency());
+			return t2;
+		}
+
 };
 
 #endif
