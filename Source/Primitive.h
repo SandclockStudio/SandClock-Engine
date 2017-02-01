62ef88d67787b74ca003f3880d1824ce62caa478
sliz3r <davidmarti.mensajes@gmail.com>
2017-02-01 11:05:27 +0100

    Added primitive.h
    Added the base class for generate primitives.

---
+++ Source/Primitive.h
@@ -0,0 +1,24 @@
+#ifndef __PRIMITIVE_H__
+#define __PRIMITIVE_H__
+
+
+class Primitive
+{
+public:
+
+	Primitive() 
+	{}
+
+	virtual ~Primitive()
+	{}
+
+	virtual void Translate()
+	{}
+
+	virtual bool Rotate()
+	{}
+
+};
+
+#endif // __PRIMITIVE_H__
+
