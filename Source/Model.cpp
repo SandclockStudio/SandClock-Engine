309dbe914a84272f658383e885e0ea002976a823
codeck1 <marc.mensajes@gmail.com>
2017-02-14 20:06:54 +0100

    Implemented Draw function for the model of the assimp

--- Source/Model.cpp
+++ Source/Model.cpp
@@ -2,6 +2,9 @@
 #include "assimp\include\assimp\cimport.h"
 #include <assimp\include\assimp\postprocess.h>
 #include <assimp\include\assimp\scene.h>
+#include "../Libraries/OpenGL/include/GL/glew.h"
+#include "Globals.h"
+#pragma comment (lib, "opengl32.lib") 
 
 
 Model::Model()
@@ -22,4 +25,18 @@ void Model::Clear()
 
 void Model::Draw()
 {
+	glBegin(GL_TRIANGLES);
+	for (int i = 0; i < scene->mNumMeshes; i++)
+	{
+		for (int j = 0; j < scene->mMeshes[i]->mNumVertices; j++)
+		{
+			glVertex3f(scene->mMeshes[i]->mVertices[j].x, scene->mMeshes[i]->mVertices[j].y, scene->mMeshes[i]->mVertices[j].z);
+			if ((scene->mMeshes[i]->HasNormals))
+				glNormal3f(scene->mMeshes[i]->mNormals[j].x, scene->mMeshes[i]->mNormals[j].y, scene->mMeshes[i]->mNormals[j].z);
+			if ((scene->mMeshes[i]->HasTextureCoords))
+				glTexCoord2f(scene->mMeshes[i]->mTextureCoords[j]->x, scene->mMeshes[i]->mTextureCoords[j]->y);
+		}
+	}
+	glEnd();
+
 }
